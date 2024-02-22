9c0a0ea6341c85851356c7c547f7eedaf0e4df2a
prasad535 <prasad.nagineni12345@gmail.com>
2024-02-23 04:05:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'visa',
+    version= '0.0.0',
+    author='Prasad Nagineni',
+    author_email='prasad.nagineni12345@gmail.com',
+    packages=find_packages()
+)
